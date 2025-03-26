df56a3f5c86dfe0a8c7aa37851a247af19cdbd6d
Joelson <joelsongsouzza@gmail.com>
2025-03-26 20:14:05 -0300

    Adicionado musica de fundo no menu

--- code/game.py
+++ code/game.py
@@ -1,14 +1,18 @@
 import pygame
 
+from code.Const import WIN_WIDTH, WIN_HEIGHT
 from code.menu import Menu
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(800, 800))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self):
+        pygame.mixer_music.load('./asset/Menu.mp3')
+        pygame.mixer_music.play(-1)
+
         while True:
             menu = Menu(self.window)
             menu.run()
