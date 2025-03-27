6975c82febb37f24e7e89d765e0d68398bc5033a
Joelson <joelsongsouzza@gmail.com>
2025-03-27 17:06:03 -0300

    Adicionado interação no Menu

--- code/menu.py
+++ code/menu.py
@@ -12,6 +12,7 @@ class Menu:
         self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
+        menu_option = 0
         pygame.mixer_music.load('./assets/menu_background_music.wav')
         pygame.mixer_music.play(-1)
 
@@ -20,7 +21,10 @@ class Menu:
             self.menu_text(50, "Basketball Game", COLOR_ORANGE, ((WIN_WIDTH / 2), 70))
 
             for i in range(len(MENU_OPTIONS)):
-                self.menu_text(50, MENU_OPTIONS[i], COLOR_WHITE, ((WIN_WIDTH / 2), (450 + (i * 50))))
+                if i == menu_option:
+                    self.menu_text(50, MENU_OPTIONS[i], COLOR_ORANGE, ((WIN_WIDTH / 2), (450 + (i * 50))))
+                else:
+                    self.menu_text(50, MENU_OPTIONS[i], COLOR_WHITE, ((WIN_WIDTH / 2), (450 + (i * 50))))
 
             pygame.display.flip()
 
@@ -28,6 +32,11 @@ class Menu:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     quit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_DOWN:
+                        menu_option = menu_option + 1 if menu_option != 2 else 0
+                    if event.key == pygame.K_UP:
+                        menu_option = menu_option - 1 if menu_option != 0 else 2
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
         text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
